38282c5a7e8b4ca4cb3d8ee7a498bae8e9f3197f
Papoune <npapon@live.fr>
2020-12-23 07:40:52 +0100

    spring le début

--- src/main/java/com/openclassrooms/testing/calcul/service/CalculatorServiceImpl.java
+++ src/main/java/com/openclassrooms/testing/calcul/service/CalculatorServiceImpl.java
@@ -9,44 +9,44 @@ import com.openclassrooms.testing.calcul.domain.model.CalculationType;
 @Named
 public class CalculatorServiceImpl implements CalculatorService {
 
-	private final Calculator calculator;
+    private final Calculator        calculator;
 
-	private final SolutionFormatter solutionFormatter;
+    private final SolutionFormatter solutionFormatter;
 
-	public CalculatorServiceImpl(Calculator calculator, SolutionFormatter solutionFormatter) {
-		this.calculator = calculator;
-		this.solutionFormatter = solutionFormatter;
-	}
+    public CalculatorServiceImpl( Calculator calculator, SolutionFormatter solutionFormatter ) {
+        this.calculator = calculator;
+        this.solutionFormatter = solutionFormatter;
+    }
 
-	@Override
-	public CalculationModel calculate(CalculationModel calculationModel) {
-		final CalculationType type = calculationModel.getType();
+    @Override
+    public CalculationModel calculate( CalculationModel calculationModel ) {
+        final CalculationType type = calculationModel.getType();
 
-		Integer response = null;
-		switch (type) {
-		case ADDITION:
-			response = calculator.add(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-			break;
-		case SUBTRACTION:
-			response = calculator.sub(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-			break;
-		case MULTIPLICATION:
-			response = calculator.multiply(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-			break;
-		case DIVISION:
-			try {
-				response = calculator.divide(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-			} catch (final ArithmeticException e) {
-				throw new IllegalArgumentException(e);
-			}
-			break;
-		default:
-			throw new UnsupportedOperationException("Unsupported calculations");
-		}
+        Integer response = null;
+        switch ( type ) {
+        case ADDITION:
+            response = calculator.add( calculationModel.getLeftArgument(), calculationModel.getRightArgument() );
+            break;
+        case SUBTRACTION:
+            response = calculator.sub( calculationModel.getLeftArgument(), calculationModel.getRightArgument() );
+            break;
+        case MULTIPLICATION:
+            response = calculator.multiply( calculationModel.getLeftArgument(), calculationModel.getRightArgument() );
+            break;
+        case DIVISION:
+            try {
+                response = calculator.divide( calculationModel.getLeftArgument(), calculationModel.getRightArgument() );
+            } catch ( final ArithmeticException e ) {
+                throw new IllegalArgumentException( e );
+            }
+            break;
+        default:
+            throw new UnsupportedOperationException( "Unsupported calculations" );
+        }
 
-		calculationModel.setSolution(response);
-		calculationModel.setFormattedSolution(solutionFormatter.format(response));
-		return calculationModel;
-	}
+        calculationModel.setSolution( response );
+        calculationModel.setFormattedSolution( solutionFormatter.format( response ) );
+        return calculationModel;
+    }
 
 }
