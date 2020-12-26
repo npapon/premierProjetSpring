849d9c4d7fa9059de568476777130fbcbaea21f7
Papoune <npapon@live.fr>
2020-12-26 12:52:50 +0100

    first commit

---
+++ src/test/java/com/openclassrooms/testing/calclul/acceptance/CucumberAIT.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.testing.caclul.acceptance;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith( Cucumber.class )
+@CucumberOptions( features = "src/test/resources/features", plugin = { "pretty", "html:target/html-cucumber-report" } )
+public class CucumberAIT {
+}
